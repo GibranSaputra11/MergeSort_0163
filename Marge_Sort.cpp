e151fef95fcbc7f9b64760acea9513ae5eb435be
GibranSaputra11 <gibranbantul@gmail.com>
2025-04-17 11:11:10 +0700

    add step 5

--- Marge_Sort.cpp
+++ Marge_Sort.cpp
@@ -80,4 +80,10 @@ void mergeSort(int low, int high)
         i++;            // Langkah 4.f.ii
         k++;            // Langkah 4.f.iii
     }
+    
+    // Langkah 5
+    for (int x = low; x <= high; x++) 
+    {
+        arr[x] = B[x];
+    }
 }
